73688f1db3b6c96f1e7ff9f4e8c0cb2661361756
Andy Jiang <andy.h.jiang@gmail.com>
2018-03-24 22:30:24 +0800

    Add flow chart.

--- userlist/consumers.py
+++ userlist/consumers.py
@@ -1,7 +1,12 @@
+# Consumers are the counterpart to views
+
 import json
 from channels import Group
 from channels.auth import channel_session_user, channel_session_user_from_http
 
+# Decorator to get the user from the Django session
+# If no logged-in user in existing session, username is empty.
+# Browser should do proper processing of visualization when username is empty.
 @channel_session_user_from_http
 def ws_connect(message):
     Group('users').add(message.reply_channel)
@@ -13,6 +18,7 @@ def ws_connect(message):
         })
     })
 
+# Decorator to get the user from the Django session
 # Leaving web page will trigger a "disconnect" message from browser.
 @channel_session_user
 def ws_disconnect(message):
